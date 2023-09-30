c601dcd95dae9fed19771acbb7cccb8c809e285a
muhammadali <101570725+yota69@users.noreply.github.com>
2023-09-30 22:23:12 +0500

    problems with mobile branch

--- src/components/pages/Catalog/RentGrid.tsx
+++ src/components/pages/Catalog/RentGrid.tsx
@@ -1,67 +1,81 @@
-import React, {useEffect, useState} from 'react';
-import {useAppSelector} from "../../../store/hooks";
-import Api, {ErrorResponse, RentResponse} from "../../../Api";
-import {Link, useSearchParams} from "react-router-dom";
-import {CarRentCard} from "../../common/CarCard";
+import React, { useEffect, useState } from "react";
+import { useAppSelector } from "../../../store/hooks";
+import Api, { ErrorResponse, RentResponse } from "../../../Api";
+import { useSearchParams } from "react-router-dom";
+import { CarRentCard } from "../../common/CarCard";
 import Loader from "../../common/Loader";
 import LoadError from "../../common/LoadError";
 import { BottomMessage } from "../CatalogPage";
 import Paginator from "../../common/Paginator";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
+import CarRequestForm from "../../common/CarRequestForm";
+import chevron from "../../../img/common/footer/chevron-for-bottom.svg";
 
-const RentGrid:React.FC<{loader?: ()=>void}> = (props) => {
+const RentGrid: React.FC<{ loader?: () => void }> = (props) => {
+	const [cars, setCars] = useState<RentResponse | ErrorResponse | undefined>(
+		undefined
+	);
 
-    const [cars, setCars] = useState<RentResponse | ErrorResponse | undefined>(undefined)
-    const filter = useAppSelector(state=>state.filter)
-    const [query, setQuery] = useSearchParams();
-    const [timer, setTimer] = useState<NodeJS.Timeout>();
-    useEffect(() => {
+	const filter = useAppSelector((state) => state.filter);
+	const [query, setQuery] = useSearchParams();
+	const [timer, setTimer] = useState<NodeJS.Timeout>();
+	useEffect(() => {
+		const fetchCarData = async () => {
+			setCars(undefined);
+			console.log("fetching cars...");
+			let carData = await Api.rent(filter, query);
 
-        const fetchCarData = async () => {
-            setCars(undefined);
-            console.log('fetching cars...')
-            let carData = await Api.rent(filter, query);
+			// if(Api.isError(carData)){
+			//     //TODO:Error check!
+			//     return;
+			// }
+			setCars(carData);
+		};
+		if (!cars) fetchCarData();
+		else {
+			clearTimeout(timer);
+			setTimer(setTimeout(fetchCarData, 1000));
+		}
+	}, [filter, query]);
+	if (!cars) return <Loader />;
+	if (Api.isError(cars)) return <LoadError response={cars} />;
+	if (cars.list.length === 0)
+		return (
+			<div
+				className={"d-flex w-100 text-center text-muted align-items-center"}
+				style={{ minHeight: "40vh" }}>
+				<em>Поиск по выбранным Вами параметрам не вернул результатов</em>
+			</div>
+		);
+	return (
+		<div>
+			<div className={"catalog__grid"}>
+				{!Api.isError(cars) &&
+					cars.list.map((i, index) => <CarRentCard car={i} key={index} />)}
+			</div>
+			<BottomMessage
+				className="bottom-message-desc"
+				button={<CarRequestForm text={"Оставить заявку"} light />}
+				text1={"Нужен автомобиль в собственность?"}
+				text2={"Подумайте о Лизинге!"}
+			/>
+			<BottomMessage
+				className="bottom-message-mobile"
+				button={
+					<CarRequestForm
+						icon={<img src={chevron} />}
+						text={"Оставить заявку"}
+						light
+					/>
+				}
+				text1={"Нужен автомобиль в собственность?"}
+				text2={"Подумайте о Лизинге!"}
+			/>
 
-            // if(Api.isError(carData)){
-            //     //TODO:Error check!
-            //     return;
-            // }
-            setCars(carData);
-        }
-        if(!cars)
-            fetchCarData();
-        else{
-            clearTimeout(timer);
-            setTimer(setTimeout(fetchCarData, 1000));
-        }
-
-    },[filter, query])
-    if(!cars)
-        return (<Loader />)
-    if(Api.isError(cars))
-        return (<LoadError response={cars} />)
-    if(cars.list.length === 0)
-        return (<div className={'d-flex w-100 text-center text-muted align-items-center'} style={{minHeight:'40vh'}}>
-            <em>Поиск по выбранным Вами параметрам не вернул результатов</em>
-        </div>)
-    return (
-        <div>
-            <div className={'catalog__grid'}>
-                {!Api.isError(cars) && cars.list.map((i, index)=>(
-                    <CarRentCard car={i} key={index} />
-                ))}
-            </div>
-            <BottomMessage button={<Link to={'/catalog'} className={'site-btn light'}>
-                Перейти&nbsp;&nbsp;<FontAwesomeIcon icon={faArrowRight} />
-            </Link>}
-                           text1={'Нужен автомобиль в собственность?'}
-                           text2={'Подумайте о Лизинге!'}/>
-            <div className={'mt-px-60'}>
-                <Paginator data={cars}/>
-            </div>
-        </div>
-    );
+			<div className={"catalog__grid-paginator"}>
+				<Paginator data={cars} />
+			</div>
+		</div>
+	);
 };
 
 export default RentGrid;
