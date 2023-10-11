e230f36ab4df3c9230c3f509babc380fa6e473db
muhammadali <101570725+yota69@users.noreply.github.com>
2023-10-12 00:37:28 +0500

    недороботки

--- src/components/common/CarCard.tsx
+++ src/components/common/CarCard.tsx
@@ -71,7 +71,25 @@ export const CarSameLink: React.FC<{
 					: "car__card-mobile-same  " + (className ?? "")
 			}>
 			<span>{text}</span>
-			<img src={caretRight} alt={""} style={{ marginLeft: "5px" }} />
+			{/* <img src={caretRight} alt={""} style={{ marginLeft: "5px" }} /> */}
+
+			<svg
+				width="15"
+				height="15"
+				viewBox="0 0 15 15"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg">
+				<g id="Chevron Right">
+					<path
+						id="Shape"
+						d="M6.25 11.25L10 7.5L6.25 3.75"
+						stroke="#222222"
+						strokeWidth="1.57642"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					/>
+				</g>
+			</svg>
 		</Link>
 	);
 };
