7ddd4f6bffcfacccf901003f51b6fff88ac8c60a
ali <muhammadalli4541@gmail.com>
2023-11-20 22:08:22 +0500

    fix payment with card

--- src/components/pages/Payment.tsx
+++ src/components/pages/Payment.tsx
@@ -10,24 +10,24 @@ const Payment = () => {
 	const checkPayment = async () => {
 		try {
 			axios
-				.get(`https://taxivoshod.ru/api/voshod-auto/?w=check-pay&pid=${pid}`)
+				.get(`https://taxivoshod.ru/api/voshod-auto/?w=check-pay&pid=${pid}` )
 				.then((res) => {
 					if (res.data.result === 1) {
-						navigate("/rent/page/1/car/1", {
+						navigate(`/rent/page/1/car/${id}`, {
 							state: {
 								status: "success",
 								payment_status: res.data.status,
 							},
 						});
 					} else {
-						navigate("/rent/page/1/car/1", {
+						navigate(`/rent/page/1/car/${id}`, {
 							state: null,
 						});
 					}
 				});
 		} catch (error) {
 			console.log(error);
-			navigate("/rent/page/1/car/1", {
+			navigate(`/rent/page/1/car/${id}`, {
 				state: null,
 			});
 		}
