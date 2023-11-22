e86d01110f8c698c4876d75cb7cfa17a9f8f5b9f
ZakharVerbik <raicords@gmail.com>
2023-11-22 14:50:21 +0300

    upd: auth context added

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,181 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { ReactNode, createContext, useEffect, useReducer } from "react";
+import {
+	AuthInitialState,
+	AuthResponce,
+	RegisterErrorType,
+} from "../types/AuthContextTypes";
+
+const actions = {
+	INITIALIZE: "INITIALIZE",
+	LOGIN: "LOGIN",
+	REGISTER: "REGISTER",
+	LOGOUT: "LOGOUT",
+};
+
+const initialState: AuthInitialState = {
+	isAuthenticated: false,
+	user_status: null,
+	has_profile: false,
+	isInitialized: false,
+	api_status: "pending",
+	error_message: null,
+}
+
+const handlers = {
+	INITIALIZE: (state: AuthInitialState, action) => {
+		const { isAuthenticated, api_status, user_status, has_profile } =
+			action.payload;
+
+		return {
+			...state,
+			isAuthenticated,
+			isInitialized: true,
+			api_status,
+			user_status,
+			has_profile,
+		};
+	},
+	LOGIN: (state:AuthInitialState, action) => {
+		const { user } = action.payload;
+
+		return {
+			...state,
+			isAuthenticated: true,
+			user,
+		};
+	},
+	LOGOUT: (state:AuthInitialState) => ({
+		...state,
+		isAuthenticated: false,
+		user: null,
+	}),
+	REGISTER: (state:AuthInitialState, action) => {
+		const { user, user_status, has_profile, error_message, api_status } =
+			action.payload;
+
+		return {
+			...state,
+			isAuthenticated: true,
+			user,
+			user_status,
+			has_profile,
+			api_status,
+			error_message,
+		};
+	},
+};
+
+const reducer = (state:AuthInitialState, action) =>
+	handlers[action.type] ? handlers[action.type](state, action) : state;
+
+export const AuthContext = createContext({
+	...initialState,
+	login: (phone: string, code: string) => Promise.resolve(),
+	loginConfirm: () => Promise.resolve(),
+	logout: () => Promise.resolve(),
+	register: (phone: string, code: string) => Promise.resolve({}),
+	registerConxfirm: () => Promise.resolve(),
+	initialize: () => Promise.resolve(),
+});
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	const initialize = async () => {
+		axios
+			.get("https://taxivoshod.ru/api/login.php", {
+				withCredentials: true,
+			})
+			.then((res: AxiosResponse<AuthResponce>) => {
+				const { success, reason, has_profile } = res.data;
+				if (success && has_profile) {
+					dispatch({
+						type: actions.INITIALIZE,
+						payload: {
+							isAuthenticated: true,
+							has_profile: true,
+							user_status: null,
+						},
+					});
+				} else {
+					dispatch({
+						type: actions.INITIALIZE,
+						payload: {
+							isAuthenticated: false,
+							user: null,
+							user_status: reason,
+							has_profile: false,
+						},
+					});
+				}
+			})
+			.catch((err) => {
+				console.log((err as AxiosError).response);
+				dispatch({
+					type: actions.INITIALIZE,
+					payload: {
+						isAuthenticated: false,
+						user: null,
+						user_status: (err as AxiosError<AuthResponce>).response?.data
+							.reason,
+						has_profile: false,
+					},
+				});
+			});
+	};
+
+	const register = async (phone: string, password: string) => {
+		try {
+			const res: AxiosResponse<AuthResponce> = await axios.get(
+				`https://taxivoshod.ru/api/login.php?auth=1&reg=1&phone=${phone}&code=${password}`,
+				{ withCredentials: true }
+			);
+
+			dispatch({
+				type: actions.REGISTER,
+				payload: {
+					isAuthenticated: true,
+					api_status: "success",
+					has_profile: false,
+					user_status: null,
+					err_message: null,
+				},
+			});
+
+			return res.data;
+		} catch (error) {
+			console.log((error as AxiosError).response);
+			dispatch({
+				type: actions.REGISTER,
+				payload: {
+					isAuthenticated: false,
+					user: null,
+					api_status: "error",
+					has_profile: false,
+					user_status: (error as AxiosError<RegisterErrorType>).response?.data
+						?.reason,
+					error_message: (error as AxiosError<RegisterErrorType>).response?.data
+						?.message,
+				},
+			});
+		}
+	};
+
+	useEffect(() => {
+		initialize().catch(console.error);
+	}, []);
+
+	return (
+		<AuthContext.Provider
+			value={{
+				...state,
+				register,
+				initialize,
+			}}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
+
+export const AuthConsumer = AuthContext.Consumer;
