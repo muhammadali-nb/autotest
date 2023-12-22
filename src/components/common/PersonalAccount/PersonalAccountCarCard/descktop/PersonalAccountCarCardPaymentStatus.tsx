622cf4efaba09f483c05b1433a94c34f44f78d36
ali <muhammadalli4541@gmail.com>
2023-12-22 23:05:48 +0500

    fix problem in desktop

--- src/components/common/PersonalAccount/PersonalAccountCarCard/descktop/PersonalAccountCarCardPaymentStatus.tsx
+++ src/components/common/PersonalAccount/PersonalAccountCarCard/descktop/PersonalAccountCarCardPaymentStatus.tsx
@@ -114,9 +114,7 @@ const PersonalAccountCarCardPaymentStatus = ({
 
 	return (
 		<div className="personal-account-rent-car_payment-status">
-			{/* {renderStatus()} */}
-			<PersonalAccountCarCardPaymentStatusWaiting status={payment_status} />
-			...
+			{renderStatus()}
 		</div>
 	);
 };
