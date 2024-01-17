9e34839db8e2b16a9bc94657a5e39efb0bc36c85
ali <muhammadalli4541@gmail.com>
2024-01-17 19:17:14 +0500

    fix bugs on leasing page and solve problems with corners on safari

--- src/components/pages/PersonalAccountLeasing/PersonalAccountLeasingCard/PersonalAccountLeasingCard.tsx
+++ src/components/pages/PersonalAccountLeasing/PersonalAccountLeasingCard/PersonalAccountLeasingCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PersonalAccountLeasingCarCardMobile from "./mobile/PersonalAccountLeasingCarCardMobile";
 import PersonalAccountLeasingCarCardPad from "./pad/PersonalAccountLeasingCarCardPad";
 import PersonalAccountLeasingCarCardDesk from "../PersonalAccountLeasingCarCardDesk";
@@ -11,21 +11,50 @@ interface IProps {
 const PersonalAccountLeasingCard = (props: IProps) => {
 	const { car } = props;
 
-	return (
-		<>
+	const [size, setSize] = useState<"desk" | "pad" | "mobile">("desk");
+
+	useEffect(() => {
+		const checkSize = () => {
+			if (window.innerWidth > 1200) {
+				setSize("desk");
+			} else if (window.innerWidth > 767) {
+				setSize("pad");
+			} else {
+				setSize("mobile");
+			}
+		};
+		window.addEventListener("resize", checkSize);
+
+		checkSize();
+
+		return () => {
+			window.removeEventListener("resize", checkSize);
+		};
+	}, []);
+
+	if (size === "desk") {
+		return (
 			<PersonalAccountLeasingCarCardDesk
 				car={car}
 				className="d-none d-xl-block"
 			/>
+		);
+	}
+
+	if (size === "pad") {
+		return (
 			<PersonalAccountLeasingCarCardPad
 				car={car}
 				className="d-none d-md-block d-xl-none"
 			/>
-			<PersonalAccountLeasingCarCardMobile
-				car={car}
-				className="d-block d-md-none"
-			/>
-		</>
+		);
+	}
+
+	return (
+		<PersonalAccountLeasingCarCardMobile
+			car={car}
+			className="d-block d-md-none"
+		/>
 	);
 };
 
