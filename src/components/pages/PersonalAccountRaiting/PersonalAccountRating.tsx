d4b2df94bfa6cdd279d31dc7899a6927f40273e3
AleksandrRyb <inkognito19931@gmail.com>
2024-02-01 14:50:00 +0300

    start making personal account raiting component

--- src/components/pages/PersonalAccountRaiting/PersonalAccountRating.tsx
+++ src/components/pages/PersonalAccountRaiting/PersonalAccountRating.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import PersonalAccountRaitingLayout from '../../layout/PersonalAccountLayout/PersonalAccountRaitingLayout';
+import PersonalAccountHeader from '../../common/PersonalAccount/PersonalAccountHeader/PersonalAccountHeader';
+import PersonalAccountData from '../PersonalAccount/PersonalAccountData';
+import { userData } from '../PersonalAccount/PersonalAccountPage';
 
-interface PersonalAccountRaitingProps {
-    props: any
-}
+const PersonalAccountRaiting = () => {
 
-
-const PersonalAccountRaiting: React.FC<PersonalAccountRaitingProps> = (props) => {
-
-    return <h1>Hello World</h1>
+    return (
+        <PersonalAccountRaitingLayout>
+            <PersonalAccountHeader>
+				<PersonalAccountData data={userData} />
+			</PersonalAccountHeader>
+            
+        </PersonalAccountRaitingLayout>
+    )
 }
 
 export default PersonalAccountRaiting;
