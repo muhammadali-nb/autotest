376ef691702c5c706bf272bae8cd12d42a17f9b7
AleksandrRyb <inkognito19931@gmail.com>
2024-02-01 13:36:56 +0300

    create start page fdor Personal account rating

---
+++ src/components/pages/PersonalAccountRaiting/PersonalAccountRating.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface IPersonalAccountRaiting {
+    props: any
+}
+
+
+const PersonalAccountRaiting: React.FC<IPersonalAccountRaiting> = (props) => {
+
+    return <h1>Hello World</h1>
+}
+
+export default PersonalAccountRaiting;
