47189d0872240a0f43a3b88498e6614e7e3ecbcb
AleksandrRyb <inkognito19931@gmail.com>
2024-02-02 17:07:20 +0300

    Create custom calendar in comon components

---
+++ src/components/common/CustomCalendar/CustomCalendar.tsx
@@ -0,0 +1,16 @@
+import { Calendar, CalendarProps } from 'react-date-range';
+
+import 'react-date-range/dist/styles.css'; 
+import 'react-date-range/dist/theme/default.css'
+import './CustomCalendar.scss';
+
+interface CustomCalendarProps extends CalendarProps {
+    isOpen: boolean;
+}
+
+const CustomCalendar: React.FC<CustomCalendarProps> = ({ isOpen, ...otherProps }) => {
+
+    return <Calendar className={isOpen ? "active" : ""} {...otherProps} />
+} 
+
+export default CustomCalendar;
